38531b2972ca14102250630f2a2521c01d4d43c8
Mike Jones <mike@westerncapelabs.com>
2016-05-19 14:47:24 +0200

    Bump version to 0.1.1 post-release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxblastsms",
-    version="0.1.0",
+    version="0.1.1",
     url='http://github.com/westerncapelabs/vumi-blastsms',
     license='BSD',
     description="An BlastSMS USSD transport for Vumi.",
--- vxblastsms/__init__.py
+++ vxblastsms/__init__.py
@@ -1,6 +1,6 @@
 """Vumi BlastSMS USSD transport."""
 from .ussd import BlastSMSUssdTransport
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ['BlastSMSUssdTransport']
