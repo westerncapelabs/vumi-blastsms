e15f0db5ba1e9ea3e2ed1e5603789528134de625
Mike Jones <mike@westerncapelabs.com>
2016-05-20 18:02:51 +0200

    release 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxblastsms",
-    version="0.1.1",
+    version="0.1.2",
     url='http://github.com/westerncapelabs/vumi-blastsms',
     license='BSD',
     description="An BlastSMS USSD transport for Vumi.",
